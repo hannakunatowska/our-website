9d3e883bbe429a709cf7415df5d1c464573ee20a
vitusberinggg <virrebering@gmail.com>
2025-09-16 15:16:47 +0200

    added function to fetch data from data.json file

---
+++ static/script.js
@@ -0,0 +1,9 @@
+fetch("static/data.json") // Fetch the frequency band data from JSON file "data.json"
+    .then(response => response.json()) // Then convert the response from JSON text to JavaScript object
+    .then(data => { // Then use the data
+        bands = data; // Store the data in the global variable "bands"
+        renderCards(bands); // Call the function to render the band cards using the data
+        renderTimeline(bands); // Call the function to render the timeline bars using the data
+    })
+
+    .catch(err => console.error("Error loading data:", err)); // Catch and log any errors that occur during the fetch process
