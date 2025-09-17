c3b29c2a0d324d341a0a249215876d76242ca212
vitusberinggg <virrebering@gmail.com>
2025-09-17 09:42:29 +0200

    added header for the fetch data function

--- static/script.js
+++ static/script.js
@@ -1,3 +1,6 @@
+
+// Loading the frequency data
+
 fetch("static/data.json") // Fetch the frequency band data from JSON file "data.json"
     .then(response => response.json()) // Then convert the response from JSON text to JavaScript object
     .then(data => { // Then use the data
